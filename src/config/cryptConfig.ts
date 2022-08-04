69e9c7b03382eae71e6be5d893ce07da1914d657
isabellacgmsa <isabellacgmsa1@gmail.com>
2022-08-04 18:26:26 -0300

    Preparando revisão de código

--- src/config/cryptConfig.ts
+++
@@ -1,4 +0,0 @@
-import "./envConfig.js"
-import Cryptr from "cryptr";
-
-export const cryptr = new Cryptr(process.env.CRYPT_KEY);
--- src/utils/testingUtil.ts
+++
@@ -1,6 +0,0 @@
-import "../config/envConfig.js"
-
-export function testingUtil(testingObject: Object){
-    if(process.env.MODE === "DEV") return testingObject;
-    return {};
-}
